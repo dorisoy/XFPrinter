93f2b9e9a9e19ce6d97aaa9737175483c066ce13
zhenghong.chen@racingandsports.com.au <zhenghong.chen@racingandsports.com.au>
2021-04-26 15:32:44 +0800

    no message

---
+++ XFPrinter/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XFPrinter.Services;
+using XFPrinter.Views;
+
+namespace XFPrinter
+{
+    public partial class App : Application
+    {
+        public static string BtAddress { get; set; } = "";
+        public static string BtName { get; set; } = "";
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XFPrinter/ViewModels/AboutViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFPrinter.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(() => 
+            {
+
+
+            });
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
